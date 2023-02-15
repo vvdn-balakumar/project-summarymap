75d7bc652bcec8cc144f970b8bb6c3b04c828ee5
leander.xVVDN <leander.x@vvdntech.in>
2023-02-15 18:14:20 +0530

    summarymapTimeTigger Added

--- summarymapTimerTrigger1/index.js
+++ summarymapTimerTrigger1/index.js
@@ -1,9 +1,11 @@
-module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+var objSummaryMap = require('./controllers/summarymapcontroller.js');
+var dbCon = require('./dao/mongodaoimpl');
+
+module.exports = function (context, myTimer) {
+    context.log('Summary map initiated');
+    objSummaryMap.getSummaryMap(context, function (err, obj) {
+        context.log('Error', err);
+       // dbCon.closeConnection();
+        context.done();
+    });
 };
